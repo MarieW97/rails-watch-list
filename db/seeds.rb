c1fa29c52d9150e1ab3135e3be9912ec6397f1bc
Marie Wright <mariedcpswright@gmail.com>
2021-11-11 17:49:47 +0000

    Controllers and Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating movies..'
+
+15.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    rating: rand(0..10),
+    poster_url: 'https://www.soda.com/wp-content/uploads/2020/04/lord-of-the-rings-streaming-guide.jpg'
+  )
+  movie.save!
+  puts "Created movie with id: #{movie.id}"
+end
+
+puts 'Finished creating movies!'
